02d76a7a03341aaeec635324509142d0bbf1230d
Naod <nayimam@gmail.com>
2022-11-16 22:33:43 -0800

    Function now evenly distributes grid along width & height of container

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const cont = document.getElementById('container'); // retreive the div with id="
  /*so this is the basic form for adding things to the container I want
    Now I want to add multiple divs to the container*/
 
-let gridDimension = 3;
+let numGrid = 30;
  
 
 /* we have the number of containers we need, 16 ** 2 = 256" *
@@ -31,7 +31,29 @@ function addSquares(num, cE, nC, pN) { // Now this function adds as many divs to
         createDiv(cE, nC, pN)    
 
     }
+    //This portion determines the necessary width of the grid to fit into the box.
+    gridDimension = 600 / num
+    gridDimension += "px"
+    var hw = document.getElementsByClassName('gridbox');
+    for(var i=0; i< hw.length;i++){
+       hw[i].style.width = gridDimension;
+       hw[i].style.height = gridDimension;
+    }
+    console.log(gridDimension)
+     
 }
 
-addSquares(gridDimension, 'div', 'gridbox', 'container');
+addSquares(numGrid, 'div', 'gridbox', 'container');
 console.log(container)
+
+/*In order to get the width and height of the squares in the grid
+  to be dependent on the number of squares and the size of the container
+  I will create a function.
+     
+    2. Divide width, 512 by the number of squares to a side
+    3. Append that width to the gridbox elements
+  
+  
+  */
+ 
+ 
