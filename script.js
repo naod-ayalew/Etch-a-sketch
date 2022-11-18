aaecd352e0d87be148f2e2ee05a3f4a73f6bd74b
Naod <nayimam@gmail.com>
2022-11-17 22:46:45 -0800

    buttons to change color and grid size complete

--- script.js
+++ script.js
@@ -1,82 +1,91 @@
 /*script.js*/
-
-/*const node = document.createElement('div');  //create the element to be inserted
-const cont = document.getElementById('container'); // retreive the div with id="container"
- cont.appendChild(node); //add the created div as a child to the container */
-
-
- /*so this is the basic form for adding things to the container I want
-   Now I want to add multiple divs to the container*/
-
-numGrid = prompt("How many squares to a side please?")
-colorS = prompt("which color do you want")
- 
-function createDiv(createdElement, newClass, parentName) {         // This function add divs to the parent, "container", all divs have class "gridbox"
+//Function that automatically adds divs with class to a specified parent
+function createDiv(createdElement, newClass, parentName) {          
     const node = document.createElement(createdElement);  //create the element to be inserted
     node.classList.add(newClass) //adds class to all the divs
     const cont = document.getElementById(parentName); // retreive the div with id="container"
     cont.appendChild(node); //add the created div as a child to the container
 }
-function addSquares(num, cE, nC, pN) { // Now this function adds as many divs to the parent container as the input requires
+//Function that adds divs to the box, if 17 is the input, 289 divs are added
+function addSquares(num, cE, nC, pN) {  
+    
     let gridCount = (num ** 2)
     for (i = 0; i < gridCount; ++i) {
         createDiv(cE, nC, pN)    
     }
-    gridDimension = 600 / num  //This portion determines the necessary width of the grid to fit into the box.
+    gridDimension = 600 / num   
     gridDimension += "px"
-    var hw = document.getElementsByClassName('gridbox');  //With this command, all the divs are listed in an array of num x num
-    for(var i=0; i< hw.length;i++){   //now each div has its width and height updated to the container width divided by number of grids to a side
-       hw[i].style.width = gridDimension;
-       hw[i].style.height = gridDimension;
+    var hw = document.getElementsByClassName('gridbox');   
+    for(var i=0; i< hw.length;i++){    
+        hw[i].style.width = gridDimension;
+        hw[i].style.height = gridDimension;
     } 
 }
-addSquares(numGrid, 'div', 'gridbox', 'container');
-
-
-/*This function finds the "target" of the mouse, and when that target
-    is one of the grid divs (which all have class gridbox), the 
-    function adds the blackout class. 
-*/
-
-function changeColor(color) {
-    addEventListener('mouseover', (event) => {
-        elem = event.target
-        if (elem.classList[0] == 'gridbox') {
-            
-            elem.style.background = color;
-        }
-    })
-    
+//Black painter
+function black() {
+    for (let i = 0; i < griddy.length; i++) {
+        griddy[i].addEventListener('mouseover', (e) => {
+        changeColor(e, 'black') 
+        })
+    } 
 }
-
-
-/*This function needs to generate random colors
-
-    Array of hexadecimal digits
-    Loop 6 times, pick a random digit 0-15, get that index from the array
-    push it to a string
-
-*/
- 
-function rainBow() {
-    let hexDigits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C' ,'D' ,'E' ,'F']
-    let randColor = ''
-    for (let i = 0; i < 6; ++i) {
-        j = Math.floor(Math.random() * 15)
-        randColor += hexDigits[j]  
+//Functions for generating random colors
+function random(num) {
+    rand = Math.floor((Math.random() * num) + 1 )
+    return rand
+}
+function randomColor(num) {
+    color = `rgb(${random(num)}, ${random(num)}, ${random(num)})`
+     
+    return color
+}
+//Rainbow Painter
+function rainbow(){
+    for (let i = 0; i < griddy.length; i++) {
+        griddy[i].addEventListener('mouseover', (e) => {
+        x = randomColor(255)
+        changeColor(e, x) 
+        })
     }
-    return randColor
+} 
+//GRAYSCALE PAINTER
+function grayscale () { 
+    for (let i = 0; i < griddy.length; i++) {
+        let shades = ['FFFFFF','#e2e2e2', '#c6c6c6', '#ABABAB', '#919191', '#777777', '#5e5e5e', '#474747', '#303030', '#1b1b1b', '#000000']
+        let j= 0
+        griddy[i].addEventListener('mouseover', (e) => {
+            j += 1
+            changeColor(e, shades[j])
+
+        })
+    } 
 }
-changeColor(colorS)
-m = rainBow()
+addSquares(20, 'div', 'gridbox', 'container')
+//Function to choose color
+btn = document.querySelector('.buttons')
+btn.addEventListener('click', (e) => {
+    color = e.target.getAttribute('data-color')
+    console.log(color)
+    if (color == 'black') {black()}
+    else if (color == 'rainbow') {rainbow()}
+    else if (color == 'grayscale') {grayscale()}
+})
+
+
+
  
+//Function that resets the box each time a new grid value is selected with the slider
+const num = document.querySelector('.slider') 
+num.addEventListener('input', () => {
+    document.querySelectorAll('.gridbox').forEach(each => each.remove())  
+    gridCount = parseInt(num.value)
+    addSquares(gridCount, 'div', 'gridbox', 'container')
+})
+
+const griddy = document.querySelectorAll('.gridbox')
+function changeColor(elementToColor, bgChange) {
+    
+    elementToColor.target.style.background = bgChange
+}
 
 
-
-
-
- 
-
- 
- 
- 
